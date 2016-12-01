da15a7cbd9c0b011a4d11ed35d61163bce2c3cb4
maangie <maangie906@gmail.com>
2016-12-01 18:22:10 +0900

    Token authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, only: []
+
+  namespace :api, { format: 'json' } do
+    namespace :v1 do
+      resource :login, only: [:create], controller: :sessions
+      resource :users, only: [:create]
+    end
+  end  
 end
